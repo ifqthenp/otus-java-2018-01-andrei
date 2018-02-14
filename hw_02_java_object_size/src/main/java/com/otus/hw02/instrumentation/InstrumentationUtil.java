dcc69c2b3c9081e243c92aa0fb75ced664fbc9f6
ifqthenp <ifqthenp@gmail.com>
2018-02-14 17:05:06 +0000

    Refactor instrumentation

--- hw_02_java_object_size/src/main/java/com/otus/hw02/instrumentation/InstrumentationUtil.java
+++ hw_02_java_object_size/src/main/java/com/otus/hw02/instrumentation/InstrumentationUtil.java
@@ -1,19 +1,28 @@
 package com.otus.hw02.instrumentation;
 
 import java.lang.instrument.Instrumentation;
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 public class InstrumentationUtil
 {
-    private static Instrumentation instrumentation;
+    private static volatile Instrumentation instrumentation;
 
-    public static void premain(String args, Instrumentation inst)
+    public static void premain(final String args, final Instrumentation inst)
     {
         instrumentation = inst;
     }
 
     public static <T> long getObjectSize(Supplier<T> supplier)
     {
+        if (instrumentation == null) {
+            throw new IllegalStateException("Agent is not initialised.");
+        }
         return instrumentation.getObjectSize(supplier.get());
+    }
+
+    public static void printInfo()
+    {
+        System.out.println(Arrays.toString(instrumentation.getAllLoadedClasses()));
     }
 }
