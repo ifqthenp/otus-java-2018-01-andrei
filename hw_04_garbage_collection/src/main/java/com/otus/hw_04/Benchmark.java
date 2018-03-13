e045f57f1c013e92efaffc31b82dd6cc6fce1ef6
Andrei Bogomja <bogomja@gmail.com>
2018-03-13 21:44:39 +0000

    Remove previous implementation of GC info collection

--- hw_04_garbage_collection/src/main/java/com/otus/hw_04/Benchmark.java
+++ hw_04_garbage_collection/src/main/java/com/otus/hw_04/Benchmark.java
@@ -22,20 +22,11 @@ public class Benchmark implements BenchmarkMBean
     void run() throws InterruptedException
     {
         while (true) {
-
             for (int i = 0; i < 6; i++) {
                 arrayList.add(String.valueOf(new char[0]));
             }
 
             arrayList.remove(arrayList.size() - 1);
-
-            try (Stream<GarbageCollectorMXBean> gcMXBeanStream = ManagementFactory.getGarbageCollectorMXBeans().stream()) {
-                gcMXBeanStream
-                    .collect(Collectors.groupingBy(
-                                     GarbageCollectorMXBean::getName,
-                                     summingLong(GarbageCollectorMXBean::getCollectionTime)))
-                    .forEach((s, aLong) -> System.out.printf("%-20s: %s min %n", s, new BigDecimal(aLong / 1000.00 / 60).setScale(6, BigDecimal.ROUND_HALF_UP)));
-            }
 
             Thread.sleep(0, 1);
         }
