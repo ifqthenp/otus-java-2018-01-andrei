d3ac0f1c54eb6bd996e142202bde9921a656c719
Andrei Bogomja <bogomja@gmail.com>
2018-03-13 21:48:33 +0000

    Refactor main method to include mbean.run() in try block and to catch OutOfMemory error

--- hw_04_garbage_collection/src/main/java/com/otus/hw_04/GCDemo.java
+++ hw_04_garbage_collection/src/main/java/com/otus/hw_04/GCDemo.java
@@ -37,13 +37,26 @@ public class GCDemo
         System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
 
         int size = 5 * 1000 * 1000;
-
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
         ObjectName name = new ObjectName("com.otus:type=Benchmark");
         Benchmark mbean = new Benchmark();
         mbs.registerMBean(mbean, name);
-
         mbean.setSize(size);
-        mbean.run();
+
+        final String dashedLine = "-------------------------";
+
+        try {
+            mbean.subscribeToGcEvents();
+            mbean.run();
+        }
+        catch (OutOfMemoryError e) {
+            final String message = "   TOTAL GC TIME (MIN)   ";
+            System.out.println(dashedLine + message + dashedLine);
+            System.out.println(Benchmark.totals);
+        }
+        finally {
+            final String message = " APPLICATION GOT CRASHED ";
+            System.out.println(dashedLine + message + dashedLine);
+        }
     }
 }
