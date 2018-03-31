50a44fca597d12b86826bef7cb45d13f1d9bf57b
Andrei Bogomja <bogomja@gmail.com>
2018-03-31 20:40:04 +0300

    Remove unused constant

--- hw_07_atm_department/src/main/java/com/otus/hw_07/classes/Denominations.java
+++ hw_07_atm_department/src/main/java/com/otus/hw_07/classes/Denominations.java
@@ -8,8 +8,6 @@ public enum Denominations
 {
     HUNDRED(100), FIFTY(50), TWENTY(20), TEN(10), FIVE(5);
 
-    private static final int DEFAULT_AMOUNT = 10;
-
     private Integer denomination;
 
     Denominations(int value)
