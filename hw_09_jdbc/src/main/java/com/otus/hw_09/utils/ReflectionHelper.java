809e82d95c92aee3d65c6d97d6c7e3d8877114e5
Andrei Bogomja <bogomja@gmail.com>
2018-04-16 10:02:55 +0100

    Add reflection utility methods to build database insert string

--- hw_09_jdbc/src/main/java/com/otus/hw_09/utils/ReflectionHelper.java
+++ hw_09_jdbc/src/main/java/com/otus/hw_09/utils/ReflectionHelper.java
@@ -5,8 +5,10 @@ import com.otus.hw_09.dataset.DataSet;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public final class ReflectionHelper
 {
@@ -42,4 +44,29 @@ public final class ReflectionHelper
 
         return map;
     }
+
+    public static <T extends DataSet> String getInsertString(final T o)
+    {
+        Class<?> cl = o.getClass();
+        Field[] fields = cl.getDeclaredFields();
+        final String className = cl.getSimpleName();
+
+        final String fieldNames = Arrays.stream(fields)
+            .map(s -> "`" + s.getName() + "`")
+            .collect(Collectors.joining(", ", "(", ")"));
+
+        final String params = Arrays.stream(fields)
+            .map(s -> "?")
+            .collect(Collectors.joining(", ", "(", ")"));
+
+        return "INSERT INTO " + className + fieldNames + " VALUES " + params;
+    }
+
+    public static <T> Field[] getObjectFields(final T o)
+    {
+        Field[] fields = o.getClass().getDeclaredFields();
+        AccessibleObject.setAccessible(fields, true);
+        return fields;
+    }
+
 }
