2aca972e027c9ea716a51947c387228c37b37919
Andrei Bogomja <bogomja@gmail.com>
2018-04-16 10:07:44 +0100

    Changes method implementation. Insert string is built with reflection and hardcoded field names no longer needed

--- hw_09_jdbc/src/main/java/com/otus/hw_09/executor/TExecutor.java
+++ hw_09_jdbc/src/main/java/com/otus/hw_09/executor/TExecutor.java
@@ -4,33 +4,29 @@ import com.otus.hw_09.dataset.DataSet;
 import com.otus.hw_09.dbService.DBServiceUpdate;
 import com.otus.hw_09.utils.ReflectionHelper;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Map;
 
 public class TExecutor extends DBServiceUpdate
 {
     public <T extends DataSet> void save(final T user)
     {
-        final Map<String, Object> fields = ReflectionHelper.getFieldNames(user);
-        final String className = user.getClass().getSimpleName();
+        Field[] fields = ReflectionHelper.getObjectFields(user);
+        final String insert = ReflectionHelper.getInsertString(user);
 
-        String name = (String) fields.getOrDefault("name", null);
-        int age = (int) fields.getOrDefault("age", null);
-
-        final String insert = "INSERT INTO " + className + "(`name`, `age`) VALUES (?, ?)";
         try (PreparedStatement stmt = getConnection().prepareStatement(insert, Statement.RETURN_GENERATED_KEYS)) {
-            stmt.setString(1, name);
-            stmt.setInt(2, age);
+            stmt.setString(1, (String) fields[0].get(user));
+            stmt.setInt(2, (Integer) fields[1].get(user));
             stmt.executeUpdate();
 
             ResultSet keys = stmt.getGeneratedKeys();
             if (keys.next()) user.setId(keys.getLong(1));
         }
-        catch (SQLException e) {
+        catch (SQLException | IllegalAccessException e) {
             e.printStackTrace();
         }
     }
