439d0b13d19f55ac39585ae0cdc54b3cd375898f
Andrei Bogomja <bogomja@gmail.com>
2018-09-13 20:27:09 +0100

    Modify deploy script

--- hw_16_multiproc_app/deploy.sh
+++ hw_16_multiproc_app/deploy.sh
@@ -2,8 +2,14 @@
 
 JETTY_HOME=/opt/jetty/jetty-9.4.12
 
-mvn clean -pl front -pl master -pl back package
+cd master && mvn clean && cd ..
+cd front && mvn clean && cd ..
+cd back && mvn clean && cd ..
+
+mvn package
+
+#java -jar master/target/master.jar
 
 # linux
-# cp front/target/front.war ${JETTY_HOME}/webapps/root.war
+cp front/target/front.war ${JETTY_HOME}/webapps/root.war
 
