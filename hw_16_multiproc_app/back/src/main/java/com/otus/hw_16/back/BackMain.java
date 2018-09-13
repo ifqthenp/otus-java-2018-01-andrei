1a21e251a93739ae719c6dd7e870ecefb90c34a3
Andrei Bogomja <bogomja@gmail.com>
2018-09-13 13:10:25 +0100

    Add clasess to setup initial client server communication

--- hw_16_multiproc_app/back/src/main/java/com/otus/hw_16/back/BackMain.java
+++ hw_16_multiproc_app/back/src/main/java/com/otus/hw_16/back/BackMain.java
@@ -1,16 +1,62 @@
 package com.otus.hw_16.back;
 
 import com.otus.hw_16.back.util.DatabaseUtil;
+import com.otus.hw_16.master.app.Msg;
+import com.otus.hw_16.master.channel.SocketMsgWorker;
+import com.otus.hw_16.master.messages.PingMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class BackMain {
 
-    public static void main(String[] args) {
+    private static final Logger logger = LoggerFactory.getLogger(BackMain.class.getName());
+
+    private static final int PORT = 5050;
+    private static final int PAUSE_MS = 5000;
+    private static final String HOST = "localhost";
+    private static final int MAX_MESSAGES_COUNT = 10;
+
+    public static void main(String[] args) throws Exception {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("classpath:spring-context.xml");
 
         DatabaseUtil.emulateLoad(context);
+
+        new BackMain().start();
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    private void start() throws Exception {
+        SocketMsgWorker client = new ClientSocketMsgWorker(HOST, PORT);
+        client.init();
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            try {
+                while (true) {
+                    Object msg = client.take();
+                    System.out.println("Message received: " + msg.toString());
+                }
+            } catch (InterruptedException e) {
+                logger.info(e.getMessage());
+            }
+        });
+
+        int count = 0;
+        while (count < MAX_MESSAGES_COUNT) {
+            Msg msg = new PingMsg();
+            client.send(msg);
+            System.out.println("Message sent: " + msg.toString());
+            Thread.sleep(PAUSE_MS);
+            count++;
+        }
+        client.close();
+        executorService.shutdown();
     }
 
 }
---
+++ hw_16_multiproc_app/back/src/main/java/com/otus/hw_16/back/ClientSocketMsgWorker.java
@@ -0,0 +1,26 @@
+package com.otus.hw_16.back;
+
+import com.otus.hw_16.master.channel.SocketMsgWorker;
+
+import java.io.IOException;
+import java.net.Socket;
+
+class ClientSocketMsgWorker extends SocketMsgWorker {
+
+    private final Socket socket;
+
+    ClientSocketMsgWorker(String host, int port) throws IOException {
+        this(new Socket(host, port));
+    }
+
+    private ClientSocketMsgWorker(Socket socket) throws IOException {
+        super(socket);
+        this.socket = socket;
+    }
+
+    public void close() throws IOException {
+        super.close();
+        socket.close();
+    }
+
+}
