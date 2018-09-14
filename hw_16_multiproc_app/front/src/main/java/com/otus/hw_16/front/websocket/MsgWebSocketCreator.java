3ce5d0d72a891e5a49462d5dc270f5935c396b0a
Andrei Bogomja <bogomja@gmail.com>
2018-09-14 15:17:51 +0100

    Remove logging

--- hw_16_multiproc_app/front/src/main/java/com/otus/hw_16/front/websocket/MsgWebSocketCreator.java
+++ hw_16_multiproc_app/front/src/main/java/com/otus/hw_16/front/websocket/MsgWebSocketCreator.java
@@ -5,12 +5,8 @@ import com.otus.hw_16.master.app.MsgWorker;
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
 import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
 import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MsgWebSocketCreator implements WebSocketCreator {
-
-    private final static Logger logger = LoggerFactory.getLogger(MsgWebSocketCreator.class.getName());
 
     private MsgWorker frontClient;
     private FrontendService frontendService;
@@ -22,7 +18,6 @@ public class MsgWebSocketCreator implements WebSocketCreator {
 
     @Override
     public Object createWebSocket(final ServletUpgradeRequest servletUpgradeRequest, final ServletUpgradeResponse servletUpgradeResponse) {
-        logger.info("Websocket created");
         return new MsgWebSocket(frontendService, frontClient);
     }
 
