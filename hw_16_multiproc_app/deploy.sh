2b53170623ccbaa8acc1a1ee3a2a8a7c48cedc84
Andrei Bogomja <bogomja@gmail.com>
2018-09-18 15:24:00 +0100

    Add check if mysql is running

--- hw_16_multiproc_app/deploy.sh
+++ hw_16_multiproc_app/deploy.sh
@@ -2,6 +2,14 @@
 
 JETTY_HOME=/opt/jetty/jetty-9.4.12
 
+if ps -aux | grep -q '[m]ysqld'; then
+    printf "%s\\n" "mysql is running..."
+else
+    printf "%s\\n" "starting mysql with Docker Compose..."
+    docker-compose up -d
+    sleep 2
+fi
+
 cd master && mvn clean && cd ..
 cd front && mvn clean && cd ..
 cd back && mvn clean && cd ..
