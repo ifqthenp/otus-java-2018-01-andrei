c6a784cc2cad1aaf311204c86ad6da870e4988f4
Andrei Bogomja <bogomja@gmail.com>
2018-09-19 12:24:53 +0100

    Add init() method to frontend service

--- hw_16_multiproc_app/front/src/main/java/com/otus/hw_16/front/frontendService/FrontendService.java
+++ hw_16_multiproc_app/front/src/main/java/com/otus/hw_16/front/frontendService/FrontendService.java
@@ -4,6 +4,8 @@ import com.otus.hw_16.front.websocket.MsgWebSocketHandler;
 
 public interface FrontendService {
 
+    void init();
+
     void handleRequestFromWebSocket(String id);
 
     void handleResponseToWebSocket(String id);
--- hw_16_multiproc_app/front/src/main/java/com/otus/hw_16/front/frontendService/FrontendServiceImpl.java
+++ hw_16_multiproc_app/front/src/main/java/com/otus/hw_16/front/frontendService/FrontendServiceImpl.java
@@ -4,10 +4,13 @@ import com.otus.hw_16.front.websocket.MsgWebSocketHandler;
 import com.otus.hw_16.master.app.Msg;
 import com.otus.hw_16.master.app.MsgWorker;
 import com.otus.hw_16.master.messages.UserDataByIdRequest;
+import com.otus.hw_16.master.messages.UserDataByIdResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FrontendServiceImpl implements FrontendService {
 
@@ -19,6 +22,23 @@ public class FrontendServiceImpl implements FrontendService {
     public FrontendServiceImpl(final MsgWebSocketHandler msgWebSocketHandler, final MsgWorker frontClient) {
         this.frontClient = frontClient;
         this.msgWebSocketHandler = msgWebSocketHandler;
+    }
+
+    @Override
+    public void init() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            try {
+                while (true) {
+                    Msg msg = frontClient.take();
+                    logger.info("Message received: {}", msg.toString());
+                    UserDataByIdResponse response = (UserDataByIdResponse) msg;
+                    handleResponseToWebSocket(response.getMessage());
+                }
+            } catch (InterruptedException e) {
+                logger.info(e.getMessage());
+            }
+        });
     }
 
     @Override
