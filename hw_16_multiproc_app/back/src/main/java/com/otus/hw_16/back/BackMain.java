799959349aa12da8dd5e15cb80f677fac61cd0a8
Andrei Bogomja <bogomja@gmail.com>
2018-09-19 23:02:12 +0100

    Add logic to handle cache update response

--- hw_16_multiproc_app/back/src/main/java/com/otus/hw_16/back/BackMain.java
+++ hw_16_multiproc_app/back/src/main/java/com/otus/hw_16/back/BackMain.java
@@ -1,21 +1,23 @@
 package com.otus.hw_16.back;
 
 import com.google.gson.GsonBuilder;
+import com.otus.hw_16.back.cacheService.CacheService;
 import com.otus.hw_16.back.dbService.DBService;
 import com.otus.hw_16.back.entities.UserDataSet;
 import com.otus.hw_16.back.util.DatabaseUtil;
+import com.otus.hw_16.back.util.MBeansUtil;
 import com.otus.hw_16.master.app.Msg;
 import com.otus.hw_16.master.channel.ClientSocketMsgWorker;
 import com.otus.hw_16.master.channel.SocketMsgWorker;
-import com.otus.hw_16.master.messages.BackClientPingMsg;
-import com.otus.hw_16.master.messages.UserDataByIdRequest;
-import com.otus.hw_16.master.messages.UserDataByIdResponse;
+import com.otus.hw_16.master.messages.*;
 import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -53,15 +55,22 @@ public class BackMain {
                     Msg msg = client.take();
                     logger.info("Message has been received on backend: {}", msg.toString());
 
+                    Msg response = null;
+
                     if (msg instanceof UserDataByIdRequest) {
                         final Long userId = Long.parseLong(((UserDataByIdRequest) msg).getMessage());
                         final UserDataSet proxiedUserDataSet = dbService.read(userId);
                         final UserDataSet userDataSet = (UserDataSet) Hibernate.unproxy(proxiedUserDataSet);
                         final String userDataJson = convertToJson(userDataSet);
-                        final UserDataByIdResponse response = new UserDataByIdResponse(userDataJson);
+                        response = new UserDataByIdResponse(userDataJson);
                         client.send(response);
-                        logger.info("Response message has been sent to frontend: {}", response.toString());
+                    } else if (msg instanceof CacheUpdateRequest) {
+                        final Map<String, Object> cacheStats = MBeansUtil.getEhCacheStats(CacheService.USER_DATASET_CACHE_NAME);
+                        final String cacheStatsJson = convertToJson(cacheStats);
+                        response = new CacheUpdateResponse(cacheStatsJson);
+                        client.send(response);
                     }
+                    logger.info("Response message has been sent to frontend: {}", Objects.requireNonNull(response).toString());
                 }
             } catch (InterruptedException e) {
                 logger.info(e.getMessage());
