7b357cca44e1168e8a6cddde70d20fab568179ad
Andrei Bogomja <bogomja@gmail.com>
2018-09-19 22:56:24 +0100

    Add public class name constants

--- hw_16_multiproc_app/master/src/main/java/com/otus/hw_16/master/app/Msg.java
+++ hw_16_multiproc_app/master/src/main/java/com/otus/hw_16/master/app/Msg.java
@@ -1,11 +1,15 @@
 package com.otus.hw_16.master.app;
 
 public abstract class Msg {
+
     public static final String CLASS_NAME_VARIABLE = "className";
+    public static final String USER_DATA_SET_BY_ID_REQUEST = "userDataByIdRequest";
+    public static final String CACHE_UPDATE_REQUEST = "cacheUpdateRequest";
 
     private final String className;
 
     protected Msg(Class<?> klass) {
         this.className = klass.getName();
     }
+
 }
