92d92176b7279a749b40f94fcdf90bf1db85d52a
Andrei Bogomja <bogomja@gmail.com>
2018-09-22 14:26:26 +0100

    Modify deploy script

--- hw_16_multiproc_app/deploy.sh
+++ hw_16_multiproc_app/deploy.sh
@@ -2,6 +2,18 @@
 
 JETTY_HOME=/opt/jetty/jetty-9.4.12
 
+cd master && mvn clean && cd ..
+cd front && mvn clean && cd ..
+cd back && mvn clean && cd ..
+
+mvn package
+
+if ps -aux | grep -q '[j]etty'; then
+    printf "%s\\n" "Jetty is running..."
+else
+    printf "%s\\n" "Jetty is not running..."
+fi
+
 if ps -aux | grep -q '[m]ysqld'; then
     printf "%s\\n" "mysql is running..."
 else
@@ -10,18 +22,5 @@ else
     sleep 2
 fi
 
-cd master && mvn clean && cd ..
-cd front && mvn clean && cd ..
-cd back && mvn clean && cd ..
-
-mvn package
-
 java -jar master/target/master.jar
 
-if ps -aux | grep -q '[j]etty'; then
-    printf "%s\\n" "Jetty is running..."
-else
-    printf "%s\\n" "starting Jetty server..."
-    ${JETTY_HOME}/bin/jetty.sh start
-    sleep 2
-fi
